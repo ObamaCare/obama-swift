8ff13a605cd1fb966369e6b8aa26c69492e49c14
Jordanbana <jordanbana@gmail.com>
2014-09-25 23:02:24 -0700

    initial files from xcode and get time function

---
+++ icountdownobama/obamatimer.swift
@@ -0,0 +1,27 @@
+//
+//  obamatimer.swift
+//  icountdownobama
+//
+//  Created by Jordan Banafsheha on 9/25/14.
+//  Copyright (c) 2014 Jordan Banafsheha. All rights reserved.
+//
+
+import Foundation
+
+//get time function
+func getCurrentTime() -> String {
+
+//get time
+let date = NSDate()
+//change format
+let formatter = NSDateFormatter()
+formatter.timeStyle = .ShortStyle
+//change to string
+var stringValue = formatter.stringFromDate(date)
+
+//print to test
+print(formatter)
+
+return stringValue
+
+}
