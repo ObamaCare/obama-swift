d6f7c259140fc8b54211a7fc8b63374fe51de886
Jordanbana <jordanbana@gmail.com>
2014-09-26 23:46:42 -0700

    got the current time to show up in the Uilabel

--- icountdownobama/ViewController.swift
+++ icountdownobama/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var clockLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+            self.clockLabel.text = getCurrentTime()
+
     }
 
     override func didReceiveMemoryWarning() {
